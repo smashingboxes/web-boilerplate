7a1135c15870cbbceae3085a8f857f51c55ce568
Zachary Kuhn <zachary.kuhn@gmail.com>
2016-06-24 16:45:24 -0400

    Remove React Transform and prefer manual handling of hot reloading.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, Router, browserHistory} from 'react-router';
 import HelloWorld from './hello-world/HelloWorld';
 
-export const Routes = () => {
+export default () => {
   return (
     <Router history={browserHistory}>
       <Route path="/" component={HelloWorld} />
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {Routes} from './Routes';
-
 import {configureStore} from './stores';
 
 const store = configureStore();
+const rootEl = document.getElementById('root');
 
-ReactDOM.render(<Provider store={store}>
-  <Routes />
-</Provider>, document.getElementById('root'));
+let render = () => {
+  const Routes = require('./Routes').default;
+  ReactDOM.render(
+    <Provider store={store}>
+      <Routes />
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  // Support hot reloading of components
+  // and display an overlay for runtime errors
+  const renderApp = render;
+  const renderError = (error) => {
+    const RedBox = require('redbox-react');
+    ReactDOM.render(
+      <RedBox error={error} />,
+      rootEl
+    );
+  };
+
+  render = () => {
+    try {
+      renderApp();
+    } catch (error) {
+      renderError(error);
+    }
+  };
+
+  module.hot.accept('./Routes', render);
+}
+
+render();
