180729d2b039b92de54431e8d6ec74d493c2a10b
Lee Allen <leeallen337@gmail.com>
2017-04-14 09:41:01 -0400

    Split out code for manifest, vendor, and app, inline the manifest

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,14 +1,24 @@
 var webpack = require('webpack');
+var InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
 var config = require('./webpack.config.base');
 
 config.bail = true;
 config.profile = false;
 config.devtool = 'source-map';
+config.output.filename = '[name].[chunkhash].js';
 
 config.plugins = config.plugins.concat([
   new webpack.LoaderOptionsPlugin({
     debug: true
   }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: (module) => {
+      return module.context && module.context.indexOf('node_modules') !== -1;
+    }
+  }),
+  new webpack.optimize.CommonsChunkPlugin({ name: 'manifest' }),
+  new InlineManifestWebpackPlugin({ name: 'webpackManifest' }),
   new webpack.optimize.UglifyJsPlugin({
     compress: {
       warnings: false
