b9c80dd71ae76455bd6f82e99bd11b2dbac1413a
Lee Allen <leeallen337@gmail.com>
2018-02-02 15:14:40 -0500

    Install and start using the prop-types package instead of importing PropTypes from react

--- modules/authentication/components/RegistrationConfirmed.jsx
+++ modules/authentication/components/RegistrationConfirmed.jsx
@@ -1,8 +1,8 @@
 import React, {
   cloneElement,
-  Component,
-  PropTypes
+  Component
 } from 'react';
+import PropTypes from 'prop-types';
 import {
   Link
 } from 'react-router';
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import Greeting from './Greeting';
 import NameTaker from './NameTaker';
