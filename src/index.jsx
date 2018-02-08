d0d785a7cd87333c2ed7bcb6d91d4f19bf2a872a
Lee Allen <leeallen337@gmail.com>
2018-02-08 17:56:45 -0500

    Update eslint config, remove lolex, upgrade axios and es6 promise

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import 'es6-promise/auto';
+
 import './css/index.css';
 
 const rootEl = document.getElementById('root');
