e96083bd3e237f2a691329b9357c81f928e25158
Lee Allen <leeallen337@gmail.com>
2018-02-09 18:09:35 -0500

    Move import of es6 promise to top of file

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
+import 'es6-promise/auto';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-
-import 'es6-promise/auto';
 
 import './css/index.css';
 
