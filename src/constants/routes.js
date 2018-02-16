a76ffab7721a767de7aa7db5e57bfc59e72e4aec
Lee Allen <leeallen337@gmail.com>
2018-02-16 17:36:35 -0500

    Add routes constants file and change route paths

---
+++ src/constants/routes.js
@@ -0,0 +1,13 @@
+const routes = {
+  AUTHENTICATION: {
+    FORGOT_PASSWORD: '/forgot-password',
+    REGISTER: '/register',
+    REGISTRATION_CONFIRMED: '/registration-confirmed',
+    RESET_PASSWORD: '/reset-password',
+    SIGN_IN: '/sign-in'
+  },
+
+  ROOT: '/'
+};
+
+export default routes;
